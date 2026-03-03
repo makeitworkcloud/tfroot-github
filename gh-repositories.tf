8754dc2cbde95cc8aec38ef7ee70d41b0f88d887
Steven Welch <steven@makeitwork.cloud>
2026-03-03 00:21:23 -0700

    feat: configure squash merge and disable force pushes (#2)

--- gh-repositories.tf
+++ gh-repositories.tf
@@ -7,9 +7,15 @@ import {
 */
 
 resource "github_repository" "repositories" {
-  for_each   = local.github_repositories
-  name       = each.key
-  visibility = var.github_visibility
+  for_each                    = local.github_repositories
+  name                        = each.key
+  visibility                  = var.github_visibility
+  allow_squash_merge          = true
+  allow_merge_commit          = true
+  allow_rebase_merge          = false
+  delete_branch_on_merge      = true
+  squash_merge_commit_title   = "PR_TITLE"
+  squash_merge_commit_message = "PR_BODY"
   lifecycle {
     ignore_changes = [
       description,
