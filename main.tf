f25cc92161dd9dfd533aa39a259e0812aecbb6b6
Steven Welch <steven@makeitwork.cloud>
2026-08-28 18:43:23 -0600

    fix: preserve existing secret lookups

--- main.tf
+++ main.tf
@@ -77,7 +77,7 @@ locals {
     }
     "onion_secret_access_key" = {
       name         = "ONION_AWS_SECRET_ACCESS_KEY"
-      value        = data.sops_file.secret_vars.data["onion_secret_access_key"]
+      value        = data.sops_file.secret_vars.data["onion_aws_secret_access_key"]
       repositories = ["www"]
     }
     "www_s3_bucket" = {
@@ -92,12 +92,12 @@ locals {
     }
     "www_access_key_id" = {
       name         = "AWS_ACCESS_KEY_ID"
-      value        = data.sops_file.secret_vars.data["www_access_key_id"]
+      value        = data.sops_file.secret_vars.data["www_aws_access_key_id"]
       repositories = ["www"]
     }
     "www_secret_access_key" = {
       name         = "AWS_SECRET_ACCESS_KEY"
-      value        = data.sops_file.secret_vars.data["www_secret_access_key"]
+      value        = data.sops_file.secret_vars.data["www_aws_secret_access_key"]
       repositories = ["www"]
     }
     "cloudflare_zone_id" = {
