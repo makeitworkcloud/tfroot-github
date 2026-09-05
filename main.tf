6def5a6fcee78f1443d7c50feeca5818aa7c7b53
Steven Welch <steven@makeitwork.cloud>
2026-09-05 10:46:27 -0600

    fix: preserve unrelated GitHub configuration

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ locals {
     "terraform-libvirt-domain" = ["cloud-init", "libvirt", "libvirt-provider", "terraform-module"]
     "tfroot-aws"               = ["aws-provider", "kms", "opentofu", "s3-backend", "sops", "tfstate"]
     "tfroot-cloudflare"        = ["cloudflare", "cloudflare-access", "cloudflare-tunnel", "opentofu", "s3-backend", "sops", "tfstate"]
-    "tfroot-gcp"               = ["gcp", "gcs-backend", "kms", "opentofu", "s3-backend", "sops", "workload-identity-federation"]
+    "tfroot-gcp"               = ["gcp", "kms", "opentofu", "s3-backend", "sops", "tfstate", "workload-identity-federation"]
     "tfroot-github"            = ["github-actions", "opentofu", "s3-backend", "sops", "tfstate", "terraform-provider-github"]
     "tfroot-libvirt"           = ["cloud-init", "k3s", "libvirt", "libvirt-provider", "opentofu", "s3-backend", "sops", "tfstate"]
     "tfroot-namecheap"         = ["cloudflare", "dns", "domains", "namecheap", "opentofu"]
@@ -108,7 +108,7 @@ locals {
     }
     "onion_secret_access_key" = {
       name         = "ONION_AWS_SECRET_ACCESS_KEY"
-      value        = data.sops_file.secret_vars.data["onion_secret_access_key"]
+      value        = data.sops_file.secret_vars.data["onion_aws_secret_access_key"]
       repositories = ["www"]
     }
     "www_s3_bucket" = {
@@ -128,7 +128,7 @@ locals {
     }
     "www_secret_access_key" = {
       name         = "AWS_SECRET_ACCESS_KEY"
-      value        = data.sops_file.secret_vars.data["www_aws_secret_access_key"]
+      value        = data.sops_file.secret_vars.data["www_secret_access_key"]
       repositories = ["www"]
     }
     "cloudflare_zone_id" = {
